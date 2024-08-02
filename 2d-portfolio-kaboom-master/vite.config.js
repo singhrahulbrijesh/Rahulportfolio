7e10265e1b6399425e21f78ae21a6907e2dbe16d
Rahul Singh <82301632+singhrahulbrijesh@users.noreply.github.com>
2024-08-02 15:44:14 -0400

    Update vite.config.js

--- 2d-portfolio-kaboom-master/vite.config.js
+++ 2d-portfolio-kaboom-master/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/Rahulportfolio/",
-  build: {
-    minify: "terser",
-  },
-});
+export default {
+  base: '/Rahulportfolio/',
+  // other configurations
+};
+
