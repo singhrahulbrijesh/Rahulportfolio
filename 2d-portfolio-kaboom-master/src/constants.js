a81483ce9416ddee914038b32be6437d42756f1a
Rahul Singh <singhrahulbrijesh@gmail.com>
2024-08-02 11:59:48 -0400

    Initial commit

---
+++ 2d-portfolio-kaboom-master/src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC, the command center for my Data Science and Machine/Deep Learning adventures. It’s where I turn coffee into code and data into insights!
+    I've made a couple of projects in that Domain. I also like languages such as Python, SQL, Javascript. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/singhrahulbrijesh" target="_blank">Github</a>!`,
+  "cis-degree": `This is my Computer Information Science degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also love to write some research works and here are some of link to my research paper. Go check out <a href="https://dl.acm.org/doi/10.1145/3644815.3644981" & <a href="file:///C:/Users/Rahul%20Singh/Downloads/IEEE_Software___Common_Defects_in_Web_Browsers.pdf" target="_blank">my research work</a> and <a href="file:///C:/Users/Rahul%20Singh/Downloads/IEEE_Software___Common_Defects_in_Web_Browsers.pdf" target="_blank">IEEE Software Paper</a>!
+  These works highlight my passion and expertise in the AI domain`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/singhrahulbrijesh/singhrahulbrijesh/blob/main/Rahul%20Singh.pdf" target="_blank">Check it out?</a>
+  Contact me at singhrahulsb@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Rahul's portfolio, just close the tab.`,
+};
